ac91125e4b1f163340c00f3c839fc31d6dc34d52
Leung-JZ <375082677@qq.com>
2018-05-10 15:57:04 +0800

    Add nuxt-buefy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,53 @@
+const path = require('path')
+
 module.exports = {
-  /*
+    /*
   ** Headers of the page
   */
-  head: {
-    title: 'cnode-nuxt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A Nuxt.js project for CNode.' }
+    head: {
+        title: 'cnode-nuxt',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'A Nuxt.js project for CNode.' }
+        ],
+        link: [
+            { rel: 'stylesheet', href: 'https://cdn.bootcss.com/MaterialDesign-Webfont/2.3.54/css/materialdesignicons.css'},
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    modules: [
+        ['nuxt-buefy', { materialDesignIcons: false }],
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
+    /**
+     ** Customize styles 
+     */
+    css: [
+        // { src: 'bulma', lang: 'sass' },
+        { src: '~assets/styles/bulma.reset.scss', lang: 'scss' },
+        // { src: '~assets/styles/common.scss', lang: 'scss' }
+    ],
     /*
-    ** Run ESLint on save
+    ** Customize the progress bar color
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    loading: { color: '#3B8070' },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** Run ESLint on save
+        */
+        extend(config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+            config.resolve.alias['@'] = path.join(__dirname)
+        }
     }
-  }
 }
